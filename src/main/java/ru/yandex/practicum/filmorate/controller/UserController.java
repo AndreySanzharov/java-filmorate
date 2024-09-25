1011779ccdd6c0fb0cbfc2eb3415f4590d0fc736
AndreySanzharov <Andrsan2004@yandex.ru>
2024-09-25 17:29:49 +0300

    сделан обработчик фильмов

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,6 +1,8 @@
 package ru.yandex.practicum.filmorate.controller;
 
 public class UserController {
+
+
 //    создание пользователя;
 //    обновление пользователя;
 //    получение списка всех пользователей.
