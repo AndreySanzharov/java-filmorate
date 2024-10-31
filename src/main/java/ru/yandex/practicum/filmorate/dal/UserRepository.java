f952a1725c80aa0f4cb166dbaa531cc41cf22e05
AndreySanzharov <Andrsan2004@yandex.ru>
2024-10-31 22:37:17 +0300

    попытка подключить dbeaver

--- src/main/java/ru/yandex/practicum/filmorate/dal/UserRepository.java
+++ src/main/java/ru/yandex/practicum/filmorate/dal/UserRepository.java
@@ -3,6 +3,7 @@ package ru.yandex.practicum.filmorate.dal;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
+import ru.yandex.practicum.filmorate.exceptions.NotFoundException;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.user.UserStorage;
 
@@ -16,6 +17,8 @@ public class UserRepository extends BaseRepository implements UserStorage {
     private static final String UPDATE_QUERY = "UPDATE USERS SET EMAIL = ?, LOGIN = ?, USERNAME = ?, " + "BIRTHDAY = ? WHERE USER_ID = ?";
     private static final String USER_BY_ID_QUERY = "SELECT * FROM USERS WHERE USER_ID = ?";
     private static final String DELETE_QUERY = "DELETE FROM USERS WHERE USER_ID = ?";
+    private static final String EXISTS_USER_QUERY = "SELECT COUNT(1) FROM USERS WHERE USER_ID = ?";
+
 
     public UserRepository(JdbcTemplate jdbc, RowMapper mapper) {
         super(jdbc, mapper);
@@ -35,6 +38,10 @@ public class UserRepository extends BaseRepository implements UserStorage {
 
     @Override
     public User update(User newUser) {
+        Integer count = jdbc.queryForObject(EXISTS_USER_QUERY, Integer.class, newUser.getId());
+        if (count == null || count == 0) {
+            throw new NotFoundException("Пользователь с ID " + newUser.getId() + " не найден");
+        }
         update(UPDATE_QUERY,
                 newUser.getEmail(),
                 newUser.getLogin(),
